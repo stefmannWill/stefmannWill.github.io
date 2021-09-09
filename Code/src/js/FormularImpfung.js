125a92306b611bdf429c3f39f8c194c899abf70f
stefa <stefa@LAPTOP-OTA0VNFH>
2021-09-09 15:03:17 +0200

    Aktuelle Version

--- Code/src/js/FormularImpfung.js
+++ Code/src/js/FormularImpfung.js
@@ -1,93 +1,81 @@
 "use strict";
- let fehlerbox=[];
-// let kontaktdaten={
-//
-// }
+// Array anlegen in dem die Felder ohne Eingabe  hinterlegt werden
+let fehlerbox=[];
+
+const meldungAnzeigen = function(e){
+  let htmlCode=`<div class="meldung" id="terminmeldung">Guten Tag ${e.Anrede} ${e.Nachname}, <br> <br> vielen Dank für Ihr Interesse an meiner Website. Natürlich können Sie keinen Termin für eine Impfung machen.</div>`;
+  if (document.querySelector("#terminmeldung")!=null) {
+    document.querySelector("#terminmeldung").remove();
+  }
+  document.querySelector("#terminImpfung .flexcont .flexbox:nth-of-type(1)").insertAdjacentHTML("afterbegin",htmlCode);
+}
+
+// Gibt eine fehlermeldung aus die Anezigt welche Felder nicht ausgefüllt wurden
+const fehlermeldungAnzeigen = function(){
+  let fehlercode="";
+  let htmlCode=`<div class="meldung" id="fehlermeldung">Die folgende Felder wurden nicht korrekt ausgefüllt: <div id="fehler"></div></div>`;
+  if (document.querySelector("#fehlermeldung")!=null) {
+    document.querySelector("#fehlermeldung").remove();
+  }
+  document.querySelector("#terminImpfung .flexcont .flexbox:nth-of-type(1)").insertAdjacentHTML("afterbegin",htmlCode);
+
+  fehlercode=fehlerbox.join(",<br> ");
+  document.querySelector("#fehler").innerHTML=fehlercode;
+  fehlerbox=[];
+}
+//Untersuchen ob alle Daten eingegeben wurden
+const datenCheck =function(daten){
+  for (let eigenschaft in daten){
+      if (daten[eigenschaft] == ""){
+        fehlerbox.push(eigenschaft);}}
+}
+//Eingegebene Daten in einem Objekt hinterlegen
+const daten_ermitteln = function(e) {
+  let stoff;
+  if(e.target.elements.biotech.checked){
+    stoff="Biontech"
+  }else if(e.target.elements.moderna.checked){
+    stoff="Moderna"
+  }else{
+    stoff="Johnson & Johnson"}
+
+  return{
+    Anrede:e.target.elements.anrede.value,
+    Vorname:e.target.elements.vorname.value,
+    Nachname:e.target.elements.nachname.value,
+    Stadt:e.target.elements.stadt.value,
+    Postleitzahl:e.target.elements.postleitzahl.value,
+    Straße:e.target.elements.straße.value,
+    Hausnummer:e.target.elements.hausnummer.value,
+    Telefonnr:e.target.elements.telefonnr.value,
+    Email:e.target.elements.email.value,
+    Krankenkasse:e.target.elements.krankenkasse.value,
+    Impfstoff:stoff,
+    Tagvorschlag_1:e.target.elements.tagvorschlag_1.value,
+    Uhrzeitvorschlag_1:e.target.elements.uhrzeitvorschlag_1.value,
+    Tagvorschlag_2:e.target.elements.tagvorschlag_2.value,
+    Uhrzeitvorschlag_2:e.target.elements.uhrzeitvorschlag_2.value,
+    Tagvorschlag_3:e.target.elements.tagvorschlag_3.value,
+    Uhrzeitvorschlag_3:e.target.elements.uhrzeitvorschlag_3.value,
+    }
+}
+
 
 let eintrag = document.querySelector("#formular_impfung");
 eintrag.addEventListener("submit",e=>{
   e.preventDefault();
+  if (document.querySelector("#fehlermeldung")!=null) {
+    document.querySelector("#fehlermeldung").remove();
+  }
+  if (document.querySelector("#terminmeldung")!=null) {
+    document.querySelector("#terminmeldung").remove();
+  }
   let formulardaten= daten_ermitteln(e);
   datenCheck(formulardaten);
-  // fensterCreate(formulardaten);
-  // fensterOffnen();
-  // console.log("hallo hallo");
-  // console.log(e);
-  // console.log(formulardaten);
-})
-
-const datenCheck =function(daten){
-  console.log(daten);
-  console.log(daten.anrede);
-  for (let eigenschaft in daten){
-      console.log(eigenschaft);
-      console.log(daten[eigenschaft]);
-      if (daten[eigenschaft] == ""){
-        fehlerbox.push(eigenschaft);
-      }
+  if(fehlerbox.length>0){
+    fehlermeldungAnzeigen();
+  }else{
+    meldungAnzeigen(formulardaten);
   }
 
-  console.log(fehlerbox);
-}
-
-const daten_ermitteln = function(e) {
-  return{
-    anrede:e.target.elements.anrede.value,
-    vorname:e.target.elements.vorname.value,
-    nachname:e.target.elements.nachname.value,
-    stadt:e.target.elements.stadt.value,
-    postleitzahl:e.target.elements.postleitzahl.value,
-    straße:e.target.elements.straße.value,
-    hausnummer:e.target.elements.hausnummer.value,
-    telefonnr:e.target.elements.telefonnr.value,
-    email:e.target.elements.email.value,
-    krankenkasse:e.target.elements.krankenkasse.value,
-    biotech:e.target.elements.biotech.checked,
-    moderna:e.target.elements.moderna.checked,
-    johnson:e.target.elements.moderna.checked,
-    astra:e.target.elements.moderna.checked,
-    tagvorschlag_1:e.target.elements.tagvorschlag_1.value,
-    uhrzeitvorschlag_1:e.target.elements.uhrzeitvorschlag_1.value,
-    tagvorschlag_2:e.target.elements.tagvorschlag_2.value,
-    uhrzeitvorschlag_2:e.target.elements.uhrzeitvorschlag_2.value,
-    tagvorschlag_3:e.target.elements.tagvorschlag_3.value,
-    uhrzeitvorschlag_3:e.target.elements.uhrzeitvorschlag_3.value,
-    nachricht:e.target.elements.nachricht.value,}
-}
-
-
-// const fensterCreate =function(daten){
-
-// let htmlcode =`<section id="terminbestätigung">
-//   <h1>Ihre Terminbestätgung</h1>
-//   <article class="">
-//     Sehr geehrter ${daten.anrede} ${daten.nachname} ,<br>
-//     <br>
-//     gerne bestätigen wir Ihnen den Impftermin am  ${daten.tagvorschlag_2} um ${daten.uhrzeitvorschlag_2}  <br>
-//   </article>
-//   </section>`;
-
-// console.log(htmlcode);
-// // document.querySelector("body").innerHTML=htmlcode;
-// // document.querySelector("body").insertAdjacentHTML("afterbegin",htmlcode)
-//
-//
-// }
-
-// const fensterOffnen=function() {
-//   // var file =`D:/ArztSeite/hallo.txt`;
-//   // console.log(file);
-//   window.open(`D:/ArztSeite/Code/Terminbestätigung.html`);
-// }
-
-const daten_verarbeiten = function(formulardaten) {
-    let wunschtermin1 =`Wunschtermin Nr.1: ${formulardaten.tagvorschlag_1} um ${formulardaten.uhrzeitvorschlag_1} Uhr`
-    let wunschtermin2 =`Wunschtermin Nr.2: ${formulardaten.tagvorschlag_2} um ${formulardaten.uhrzeitvorschlag_2} Uhr`
-    let wunschtermin3 =`Wunschtermin Nr.3: ${formulardaten.tagvorschlag_3} um ${formulardaten.uhrzeitvorschlag_3} Uhr`
-
-    console.log(wunschtermin1);
-    // return{
-    //
-    //
-    // }
-}
+})
